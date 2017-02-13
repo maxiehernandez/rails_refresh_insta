940701f751029917b0c3566e7155310ed8f88df3
Maxie <maxinehernandez@hotmail.com>
2017-02-13 15:28:45 -0500

    setting up authentication with bcrypt

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,19 @@
+class SessionsController < ApplicationController
+  def new
+   @post = Post.all.limit(8)
+  end
+
+  def create
+   @user = User.find_by_username(params[:username])
+   # If the user exists AND the password entered is correct.
+   if @user && @user.authenticate(params[:password])
+     session[:user_id] = @user.id
+   else
+     render json: @user.errors, status: :unprocessable_entity
+   end
+  end
+
+  def destroy
+   session[:user_id] = nil
+  end
+end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -27,6 +27,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
+      session[:user_id] = user.id
       render json: @user
     else
       render json: @user.errors, status: :unprocessable_entity
