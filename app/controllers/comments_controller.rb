6883e321511d01a0e7043c80ffd4a8757a08090f
Maxie <maxinehernandez@hotmail.com>
2017-02-22 18:38:49 -0500

    updated comments - WIP

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -7,6 +7,7 @@ class CommentsController < ApplicationController
   end
 
   def create
+    puts  "comment create method params iiiiiiiiiiiiiii", params.inspect, " oooo"
     @comment = @post.comments.build(comment_params)
 
     if @comment.save!
@@ -26,10 +27,10 @@ class CommentsController < ApplicationController
     end
 
     def set_post
-      @post = Post.find(params[:post_id])
+      @post = Post.find(params[:data][:relationships][:post][:data][:id])
     end
 
     def comment_params
-      params.require(:comment).permit(:body, :user_id)
+      params.require(:comment).permit(:body)
     end
 end
--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -5,11 +5,7 @@ class SearchesController < ApplicationController
     @posts = Post.includes(:comments).all
 
     search_query = params[:q].downcase
-    render json: @posts.select { |post|
-      if !post.file_name.nil?
-        post.caption.downcase.include?(search_query) || post.file_name.downcase.include?(search_query)
-      end
-    }
+    render json: @posts.select { |post| post.caption.downcase.include?(search_query) || post.file_name.downcase.include?(search_query) }
 
     # render json: StoreIndex.query(query_string: {fields: [:caption, :original_file_name, :file_name, :body], query: params[:q], default_operator: 'or'}).load.to_a, each_serializer: SearchesSerializer, root: "searches"
   end
